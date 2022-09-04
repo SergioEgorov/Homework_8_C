f00d5d0726ead41ac0737a0373c5c7f2b0967e7d
Sergey <hojalata@mail.ru>
2022-09-04 22:41:27 +0300

    Выполнил задание №56

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -23,33 +23,33 @@ PrintArray(array);
 
 void OrderArrayLines(int[,] array)
 {
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-      for (int k = 0; k < array.GetLength(1) - 1; k++)
-      {
-        if (array[i, k] < array[i, k + 1])
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-          int temp = array[i, k + 1];
-          array[i, k + 1] = array[i, k];
-          array[i, k] = temp;
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
         }
-      }
     }
-  }
 }
 
 
 void GetArray(int[,] array)
 {
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-      array[i, j] = new Random().Next(11);
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(11);
+        }
     }
-  }
 }
 
 void PrintArray(int[,] matrix)
